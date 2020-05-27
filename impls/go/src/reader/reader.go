b9f18af1b840281d5760c65d79da171c7c41a05e
GoLangsam <GoLangsam.APA@a-bc.net>
2020-05-27 10:47:49 +0630

    go fmt

--- impls/go/src/reader/reader.go
+++ impls/go/src/reader/reader.go
@@ -66,14 +66,14 @@ func read_atom(rdr Reader) (MalType, error) {
 		}
 		return i, nil
 	} else if match, _ :=
-		  regexp.MatchString(`^"(?:\\.|[^\\"])*"$`, *token); match {
+		regexp.MatchString(`^"(?:\\.|[^\\"])*"$`, *token); match {
 		str := (*token)[1 : len(*token)-1]
 		return strings.Replace(
 			strings.Replace(
-			 strings.Replace(
-			  strings.Replace(str, `\\`, "\u029e", -1),
-			  `\"`, `"`, -1),
-			 `\n`, "\n", -1),
+				strings.Replace(
+					strings.Replace(str, `\\`, "\u029e", -1),
+					`\"`, `"`, -1),
+				`\n`, "\n", -1),
 			"\u029e", "\\", -1), nil
 	} else if (*token)[0] == '"' {
 		return nil, errors.New("expected '\"', got EOF")
